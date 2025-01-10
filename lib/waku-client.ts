6e1cacac157961015bf4f952a86e58f4ff47b69d
Radoslaw Kaminski <radoslaw@cheqd.io>
2025-01-10 12:32:39 +0000

    add more basic tests

--- lib/waku-client.ts
+++ lib/waku-client.ts
@@ -3,6 +3,55 @@ export class WakuClient {
 
   public async getDebugInfo() {
     const response = await fetch(`${this.baseUrl}/debug/v1/info`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const json = await response.json();
+
+    return json;
+  }
+
+  public async subscribeTopics(topics: string[]) {
+    const response = await fetch(`${this.baseUrl}/relay/v1/auto/subscriptions`, {
+      method: 'post',
+      headers: { accept: 'text/plain', 'content-type': 'application/json' },
+      body: JSON.stringify(topics),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return response.status;
+  }
+
+  public async publishMessage(payload: string, topic: string) {
+    const response = await fetch(`${this.baseUrl}/relay/v1/auto/messages`, {
+      method: 'post',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        payload,
+        contentTopic: topic,
+        timestamp: 0,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return response.status;
+  }
+
+  public async getMessages(topic: string) {
+    const response = await fetch(`${this.baseUrl}/relay/v1/auto/messages/${encodeURIComponent(topic)}`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
     const json = await response.json();
 
     return json;
---
+++ tests/basic.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, test } from 'bun:test';
+import { WakuClient } from '../lib/waku-client';
+
+describe('Basic Node Operation', () => {
+  const wakuClient = new WakuClient('http://0.0.0.0:21161');
+  const topic = '/my-app/2/chatroom-1/proto';
+  const payload = 'UmVsYXkgd29ya3MhIQ==';
+
+  test('1. Verify Node Information', async () => {
+    const response = await wakuClient.getDebugInfo();
+    const { enrUri } = response;
+
+    expect(enrUri).toStartWith('enr:');
+  });
+
+  test('2. Subscribe to a Topic', async () => {
+    const status = await wakuClient.subscribeTopics([topic]);
+
+    expect(status).toBe(200);
+  });
+
+  test('3. Publish a Message', async () => {
+    const status = await wakuClient.publishMessage(payload, topic);
+
+    expect(status).toBe(200);
+  });
+
+  test('4. Confirm Message Publication', async () => {
+    const response = await wakuClient.getMessages(topic);
+    const [message] = response;
+
+    expect(response).toBeArrayOfSize(1);
+    expect(message).toMatchObject({ contentTopic: topic, payload });
+  });
+});
